e68abc06c6af2af4d9618c0476c1334862546e9e
shoharto <bayes@graduate.utm.my>
2023-05-29 21:19:09 +0800

    first commit

---
+++ models/productModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+    },
+    color: {
+      type: String,
+      enum: ["red", "blue", "green"], // Add your color options here
+      required: true,
+    },
+    size: {
+      type: String,
+      enum: ["small", "medium", "large"], // Add your size options here
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Products", productSchema);
